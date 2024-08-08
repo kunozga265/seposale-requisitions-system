8823fb7ae6b56e2d6b8f81970cc190e6301b84f6
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2024-08-08 13:49:27 +0200

    Order client names properly

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,7 +13,7 @@ class ClientController extends Controller
 {
     public function index(Request $request)
     {
-        $clients = Client::orderBy("name", "desc")->paginate(100);
+        $clients = Client::orderBy("name", "asc")->paginate(100);
 
 
         if ((new AppController())->isApi($request))
