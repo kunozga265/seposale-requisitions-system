f66f14673b6afc6aeb3db6ea53864c372e928390
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-08-27 10:35:55 +0200

    force return type for organisation

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -21,7 +21,7 @@ class ClientResource extends JsonResource
             "address" => $this->address,
             "phoneNumber" => $this->phone_number != null ? "+{$this->phone_number}" : "",
             "phoneNumberOther" => $this->phone_number_other != null ? "+{$this->phone_number_other}" : "",
-            "organisation" => intval($this->organisation),
+            "organisation" => intval($this->organisation) == 1,
             "alias" => $this->alias,
             "type" => $this->type,
             "sales" => SaleResource::collection($this->sales),
