b66335142d9e21b35631a46e5474f48715095c2f
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-09-12 14:52:35 +0200

    added account resource

--- app/Http/Controllers/API/ReceiptController.php
+++ app/Http/Controllers/API/ReceiptController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AccountingAccountResource;
 use App\Models\AccountingAccount;
 use App\Models\PaymentMethod;
 use Illuminate\Http\Request;
@@ -16,7 +17,7 @@ class ReceiptController extends Controller
         return response()->json(
             [
                 "paymentMethods" => $payment_methods,
-                "accounts" => $accounts
+                "accounts" => AccountingAccountResource::collection($accounts)
             ]
         );
         
