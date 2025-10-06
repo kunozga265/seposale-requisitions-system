ec8d490755fb08e7306320520a259439ea3927c7
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-10-06 11:13:29 +0200

    getzeroed number

--- app/Http/Resources/API/ReceiptResource.php
+++ app/Http/Resources/API/ReceiptResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Resources\API;
 
-use App\Http\Controllers\API\AppController;
+use App\Http\Controllers\AppController;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ReceiptResource extends JsonResource
