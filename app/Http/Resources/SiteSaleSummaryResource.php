73d9d6e61f29be856a739c1d08cce83f5b27a495
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-10-10 12:11:24 +0200

    lite code

--- app/Http/Resources/SiteSaleSummaryResource.php
+++ app/Http/Resources/SiteSaleSummaryResource.php
@@ -40,7 +40,7 @@ class SiteSaleSummaryResource extends JsonResource
             "delivery" => $this->delivery != null ? [
                 "id" => intval($this->delivery->id),
                 "status" => intval($this->delivery->status),
-                "code" => $this->delivery->code,
+                "code" => (new AppController())->getZeroedNumber($this->delivery->code),
             ] : null,
             "overdue" => $this->delivery != null ? $this->delivery->overdue() : false,
             'profit' => floatval($this->profit()),
--- app/Http/Resources/SummaryResource.php
+++ app/Http/Resources/SummaryResource.php
@@ -38,7 +38,7 @@ class SummaryResource extends JsonResource
             "delivery" => $this->delivery != null ? [
                 "id" => intval($this->delivery->id),
                 "status" => intval($this->delivery->status),
-                "code" => $this->delivery->code,
+               "code" => (new AppController())->getZeroedNumber($this->delivery->code),
             ] : null,
             "overdue" => $this->delivery != null ? $this->delivery->overdue() : false,
             "status" => intval($this->status),
