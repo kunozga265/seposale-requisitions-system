ca8a095530982214e277431d8f3944709caa847f
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-11-04 08:06:27 +0200

    added summary to delivery note resource

--- app/Http/Resources/DeliveryNoteResource.php
+++ app/Http/Resources/DeliveryNoteResource.php
@@ -27,6 +27,7 @@ class DeliveryNoteResource extends JsonResource
             "photo" => $this->photo,
             "recipientName" => $this->recipient_name,
             "recipientPhoneNumber" => $this->recipient_phone_number,
+            "summary" => new SummaryResource($this->delivery->summary),
             "delivery" => [
                 "id" => intval($this->delivery->id),
                 "status" => intval($this->delivery->status),
