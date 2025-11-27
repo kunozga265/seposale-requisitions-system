2597f0bf3b8c03105d0e6ca7485adb7b90b490fe
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-11-27 14:53:37 +0200

    parse float

--- resources/js/Components/Payable.vue
+++ resources/js/Components/Payable.vue
@@ -214,14 +214,14 @@ export default {
         getTotal() {
             let sum = 0;
             for (let x in this.payable) {
-                sum += this.numberWithCommas(this.payable[x].total)
+                sum += parseFloat(this.payable[x].total)
             }
             return sum
         },
         getItemsTotal() {
             let sum = 0;
             for (let x in this.form.payables) {
-                sum += this.numberWithCommas(this.form.payables[x].total)
+                sum += parseFloat(this.form.payables[x].total)
             }
             return sum
         }
