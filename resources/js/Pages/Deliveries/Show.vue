6acd40367ce35b16f54c13a193922e089f0ef1ed
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2026-06-02 10:58:56 +0200

    delivery can be completed

--- resources/js/Pages/Deliveries/Show.vue
+++ resources/js/Pages/Deliveries/Show.vue
@@ -518,7 +518,7 @@
                 <secondary-button @click.native="completeDialog = false">
                     close
                 </secondary-button>
-                <primary-button v-show="delivery.data.status === 2 && payableValidation" @click.native="complete">
+                <primary-button v-show="payableValidation" @click.native="complete">
                     <svg v-show="form.processing" role="status" class="inline w-4 h-4 mr-3 text-white animate-spin"
                         viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
